7c109093eabd4176e5bffba592f36ddbc5c75420
swetabh <swetabh@nanamuna.com>
2020-10-25 23:57:43 +0530

    project moved to github

---
+++ backend/routes/exercises.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const Exercise = require('../models/exercise.model');
+
+router.route('/').get((req,res) => {
+    Exercise.find()
+    .then(exercises => res.json(exercises))
+    .catch(err => res.status(400).json('Error' + err));
+});
+
+router.route('/add').post((req,res) =>{
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newExercise = new Exercise({
+        username,
+        description,
+        duration,
+        date
+    });
+
+    newExercise.save()
+    .then(() => res.json('exercise added'))
+    .catch((err) => res.json('error' + err));
+});
+
+router.route('/:id').get((req,res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('error '+err));
+
+});
+
+router.route('/:id').delete((req,res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(exercise => res.json('exercise deleted'))
+    .catch(err => res.status(400).json('error '+err));
+
+});
+
+router.route('/update/:id').post((req,res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.date = Date.parse(req.body.date);
+        exercise.duration = Number(req.body.duration);
+
+        exercise.save()
+        .then(() => res.json('exercise updated'))
+        .catch(err => res.status(400).json('error' + err));
+    })
+    .catch(err => res.status(400).json('error' + err));
+
+});
+
+
+
+
+module.exports = router;
+
